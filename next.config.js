b5a8570f5dcf71358e4baf89182f23fb90692f8e
ninomiyadaisuke <daisukeninomiya0375@gmail.com>
2023-12-27 09:06:27 +0900

    deploy

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-		remotePatterns: [
-			{
-				protocol: "http",
-				hostname: "localhost",
-			},
-			{
-				protocol: "https",
-				hostname: "digital-hippo-production-ff84.up.railway.app",
-			},
-		],
-	},
-}
+    domains: ["localhost", "digital-hippo-production-ff84.up.railway.app"],
+    // remotePatterns: [
+    // 	{
+    // 		protocol: "http",
+    // 		hostname: "localhost",
+    // 	},
+    // 	{
+    // 		protocol: "https",
+    // 		hostname: "digital-hippo-production-ff84.up.railway.app",
+    // 	},
+    // ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
